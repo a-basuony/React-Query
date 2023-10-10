d2f5a21b8aa6a94d93bc12071302b454d61c2775
Ahmed Basuony <99911074+a-basuony@users.noreply.github.com>
2023-10-10 15:50:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,29 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Navigate to="/events" />,
-  },
-  {
-    path: "/events",
-    element: <Events />,
+  //   path: "/",
+  //   element: <Navigate to="/events" />,
+  // },
+  // {
+  //   path: "/events",
+  //   element: <Events />,
 
-    children: [
-      {
-        path: "/events/new",
-        element: <NewEvent />,
-      },
-    ],
-  },
-  {
-    path: "/events/:id",
-    element: <EventDetails />,
-    children: [
-      {
-        path: "/events/:id/edit",
-        element: <EditEvent />,
-      },
-    ],
+  //   children: [
+  //     {
+  //       path: "/events/new",
+  //       element: <NewEvent />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: "/events/:id",
+  //   element: <EventDetails />,
+  //   children: [
+  //     {
+  //       path: "/events/:id/edit",
+  //       element: <EditEvent />,
+  //     },
+  //   ],
   },
 ]);
 
